93c8b4bc5210595deccd136bc0d42070ffbf8724
AlexLundin <aml140830@utdallas.edu>
2018-10-05 02:45:01 -0500

    DATES

--- 1.2-SE-4347.501-DB/ModelsForClass/5.6/DEVELOPER/INSERTS.sql
+++ 1.2-SE-4347.501-DB/ModelsForClass/5.6/DEVELOPER/INSERTS.sql
@@ -9,13 +9,13 @@ INSERT INTO WORKS_ON (Essn, Pno, Hours)
 INSERT INTO DEPARTMENT (Dname, Dnumber, Mgr_ssn, Mgr_start_date) 
 INSERT INTO DEPENDENT (Essn, Dependent_name, Sex, Bdate, Relationship)
 INSERT INTO DEPT_LOCATIONS (Dnumber, Dlocation) VALUES (1, 'Houston');
-INSERT INTO EMPLOYEE (Fname, Minit, Lname, Ssn, Bdate, Address, Sex, Salary, Super_ssn, Dno) VALUES('John', 'B', 'Smith', 123456789, '09-JAN-1985', 'Houston, TX', 'M', 30000, 333445555, 5);
-INSERT INTO EMPLOYEE (Fname, Minit, Lname, Ssn, Bdate, Address, Sex, Salary, Super_ssn, Dno) VALUES('Franklin', 'T', 'Wong', 333445555, '09-JAN-1985', 'Houston, TX', 'M', 30000, 333445555, 5);
-INSERT INTO EMPLOYEE (Fname, Minit, Lname, Ssn, Bdate, Address, Sex, Salary, Super_ssn, Dno) VALUES('Alicia', 'J', 'Zelaya', 999887777, '09-JAN-1985', 'Houston, TX', 'M', 30000, 333445555, 5);
-INSERT INTO EMPLOYEE (Fname, Minit, Lname, Ssn, Bdate, Address, Sex, Salary, Super_ssn, Dno) VALUES('Jennifer', 'S', 'Wallace', 987654321, '09-JAN-1985', 'Houston, TX', 'M', 30000, 333445555, 5);
-INSERT INTO EMPLOYEE (Fname, Minit, Lname, Ssn, Bdate, Address, Sex, Salary, Super_ssn, Dno) VALUES('Ramesh', 'K', 'Narayan', 666884444, '09-JAN-1985', 'Houston, TX', 'M', 30000, 333445555, 5);
-INSERT INTO EMPLOYEE (Fname, Minit, Lname, Ssn, Bdate, Address, Sex, Salary, Super_ssn, Dno) VALUES('Joyce', 'A', 'English', 453453453, '09-JAN-1985', 'Houston, TX', 'M', 30000, 333445555, 5);
-INSERT INTO EMPLOYEE (Fname, Minit, Lname, Ssn, Bdate, Address, Sex, Salary, Super_ssn, Dno) VALUES('Ahmad', 'V', 'Jabbar', 987987987, '09-JAN-1985', 'Houston, TX', 'M', 30000, 333445555, 5);
-INSERT INTO EMPLOYEE (Fname, Minit, Lname, Ssn, Bdate, Address, Sex, Salary, Super_ssn, Dno) VALUES('James', 'E', 'Borg', 888665555, '09-JAN-1985', 'Houston, TX', 'M', 30000, 333445555, 5);
+INSERT INTO EMPLOYEE (Fname, Minit, Lname, Ssn, Bdate, Address, Sex, Salary, Super_ssn, Dno) VALUES('John', 'B', 'Smith', 123456789, '09-JAN-1965', 'Houston, TX', 'M', 30000, 333445555, 5);
+INSERT INTO EMPLOYEE (Fname, Minit, Lname, Ssn, Bdate, Address, Sex, Salary, Super_ssn, Dno) VALUES('Franklin', 'T', 'Wong', 333445555, '08-DEC-1955', 'Houston, TX', 'M', 30000, 333445555, 5);
+INSERT INTO EMPLOYEE (Fname, Minit, Lname, Ssn, Bdate, Address, Sex, Salary, Super_ssn, Dno) VALUES('Alicia', 'J', 'Zelaya', 999887777, '19-JAN-1968', 'Houston, TX', 'M', 30000, 333445555, 5);
+INSERT INTO EMPLOYEE (Fname, Minit, Lname, Ssn, Bdate, Address, Sex, Salary, Super_ssn, Dno) VALUES('Jennifer', 'S', 'Wallace', 987654321, '20-JUN-1941', 'Houston, TX', 'M', 30000, 333445555, 5);
+INSERT INTO EMPLOYEE (Fname, Minit, Lname, Ssn, Bdate, Address, Sex, Salary, Super_ssn, Dno) VALUES('Ramesh', 'K', 'Narayan', 666884444, '15-SEP-1962', 'Houston, TX', 'M', 30000, 333445555, 5);
+INSERT INTO EMPLOYEE (Fname, Minit, Lname, Ssn, Bdate, Address, Sex, Salary, Super_ssn, Dno) VALUES('Joyce', 'A', 'English', 453453453, '31-JUL-1972', 'Houston, TX', 'M', 30000, 333445555, 5);
+INSERT INTO EMPLOYEE (Fname, Minit, Lname, Ssn, Bdate, Address, Sex, Salary, Super_ssn, Dno) VALUES('Ahmad', 'V', 'Jabbar', 987987987, '29-MAR-1969', 'Houston, TX', 'M', 30000, 333445555, 5);
+INSERT INTO EMPLOYEE (Fname, Minit, Lname, Ssn, Bdate, Address, Sex, Salary, Super_ssn, Dno) VALUES('James', 'E', 'Borg', 888665555, '11-NOV-1937', 'Houston, TX', 'M', 30000, 333445555, 5);
 INSERT INTO PROJECT (Pname, Pnumber, Plocation, Dnum)
 INSERT INTO WORKS_ON (Essn, Pno, Hours)
