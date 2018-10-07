3a455cd96fcd740933c84712c7928f84cb81509e
AlexLundin <aml140830@utdallas.edu>
2018-10-07 03:16:05 -0500

    INSERTS

--- 1.2-SE-4347.501-DB/ModelsForClass/5.6/DEVELOPER/INSERTS.sql
+++ 1.2-SE-4347.501-DB/ModelsForClass/5.6/DEVELOPER/INSERTS.sql
@@ -6,16 +6,42 @@ INSERT INTO PROJECT (Pname, Pnumber, Plocation, Dnum)
 INSERT INTO WORKS_ON (Essn, Pno, Hours)
 
 
-INSERT INTO DEPARTMENT (Dname, Dnumber, Mgr_ssn, Mgr_start_date) 
+INSERT INTO DEPARTMENT (Dname, Dnumber, Mgr_ssn, Mgr_start_date) VALUES ('Research', 5, 333445555, '22-MAY-1988');
+INSERT INTO DEPARTMENT (Dname, Dnumber, Mgr_ssn, Mgr_start_date) VALUES ('Administration', 4, 987654321, '01-JAN-1995');
+INSERT INTO DEPARTMENT (Dname, Dnumber, Mgr_ssn, Mgr_start_date) VALUES ('Headquarters', 1, 888665555, '19-JUL-1981');
 INSERT INTO DEPENDENT (Essn, Dependent_name, Sex, Bdate, Relationship)
 INSERT INTO DEPT_LOCATIONS (Dnumber, Dlocation) VALUES (1, 'Houston');
-INSERT INTO EMPLOYEE (Fname, Minit, Lname, Ssn, Bdate, Address, Sex, Salary, Super_ssn, Dno) VALUES('John', 'B', 'Smith', 123456789, '09-JAN-1965', 'Houston, TX', 'M', 30000, 333445555, 5);
-INSERT INTO EMPLOYEE (Fname, Minit, Lname, Ssn, Bdate, Address, Sex, Salary, Super_ssn, Dno) VALUES('Franklin', 'T', 'Wong', 333445555, '08-DEC-1955', 'Houston, TX', 'M', 30000, 333445555, 5);
-INSERT INTO EMPLOYEE (Fname, Minit, Lname, Ssn, Bdate, Address, Sex, Salary, Super_ssn, Dno) VALUES('Alicia', 'J', 'Zelaya', 999887777, '19-JAN-1968', 'Houston, TX', 'M', 30000, 333445555, 5);
-INSERT INTO EMPLOYEE (Fname, Minit, Lname, Ssn, Bdate, Address, Sex, Salary, Super_ssn, Dno) VALUES('Jennifer', 'S', 'Wallace', 987654321, '20-JUN-1941', 'Houston, TX', 'M', 30000, 333445555, 5);
-INSERT INTO EMPLOYEE (Fname, Minit, Lname, Ssn, Bdate, Address, Sex, Salary, Super_ssn, Dno) VALUES('Ramesh', 'K', 'Narayan', 666884444, '15-SEP-1962', 'Houston, TX', 'M', 30000, 333445555, 5);
-INSERT INTO EMPLOYEE (Fname, Minit, Lname, Ssn, Bdate, Address, Sex, Salary, Super_ssn, Dno) VALUES('Joyce', 'A', 'English', 453453453, '31-JUL-1972', 'Houston, TX', 'M', 30000, 333445555, 5);
-INSERT INTO EMPLOYEE (Fname, Minit, Lname, Ssn, Bdate, Address, Sex, Salary, Super_ssn, Dno) VALUES('Ahmad', 'V', 'Jabbar', 987987987, '29-MAR-1969', 'Houston, TX', 'M', 30000, 333445555, 5);
-INSERT INTO EMPLOYEE (Fname, Minit, Lname, Ssn, Bdate, Address, Sex, Salary, Super_ssn, Dno) VALUES('James', 'E', 'Borg', 888665555, '11-NOV-1937', 'Houston, TX', 'M', 30000, 333445555, 5);
-INSERT INTO PROJECT (Pname, Pnumber, Plocation, Dnum)
-INSERT INTO WORKS_ON (Essn, Pno, Hours)
+INSERT INTO DEPT_LOCATIONS (Dnumber, Dlocation) VALUES (4, 'Stafford');
+INSERT INTO DEPT_LOCATIONS (Dnumber, Dlocation) VALUES (5, 'Bellaire');
+INSERT INTO DEPT_LOCATIONS (Dnumber, Dlocation) VALUES (5, 'Sugarland');
+INSERT INTO DEPT_LOCATIONS (Dnumber, Dlocation) VALUES (5, 'Houston');
+INSERT INTO EMPLOYEE (Fname, Minit, Lname, Ssn, Bdate, Address, Sex, Salary, Super_ssn, Dno) VALUES('John', 'B', 'Smith', 123456789, '09-JAN-1965', '731 Fondren, Houston, TX', 'M', 30000, 333445555, 5);
+INSERT INTO EMPLOYEE (Fname, Minit, Lname, Ssn, Bdate, Address, Sex, Salary, Super_ssn, Dno) VALUES('Franklin', 'T', 'Wong', 333445555, '08-DEC-1955', '638 Voss, Houston, TX', 'M', 40000, 888665555, 5);
+INSERT INTO EMPLOYEE (Fname, Minit, Lname, Ssn, Bdate, Address, Sex, Salary, Super_ssn, Dno) VALUES('Alicia', 'J', 'Zelaya', 999887777, '19-JAN-1968', '3321 Castle, Spring, TX', 'F', 25000, 987654321, 4);
+INSERT INTO EMPLOYEE (Fname, Minit, Lname, Ssn, Bdate, Address, Sex, Salary, Super_ssn, Dno) VALUES('Jennifer', 'S', 'Wallace', 987654321, '20-JUN-1941', '291 Berry, Bellaire, TX', 'F', 43000, 888665555, 4);
+INSERT INTO EMPLOYEE (Fname, Minit, Lname, Ssn, Bdate, Address, Sex, Salary, Super_ssn, Dno) VALUES('Ramesh', 'K', 'Narayan', 666884444, '15-SEP-1962', '975 Fire Oak, Humble, TX', 'M', 38000, 333445555, 5);
+INSERT INTO EMPLOYEE (Fname, Minit, Lname, Ssn, Bdate, Address, Sex, Salary, Super_ssn, Dno) VALUES('Joyce', 'A', 'English', 453453453, '31-JUL-1972', '5631 Rice, Houston, TX', 'F', 25000, 333445555, 5);
+INSERT INTO EMPLOYEE (Fname, Minit, Lname, Ssn, Bdate, Address, Sex, Salary, Super_ssn, Dno) VALUES('Ahmad', 'V', 'Jabbar', 987987987, '29-MAR-1969', '980 Dallas, Houston, TX', 'M', 25000, 987654321, 4);
+INSERT INTO EMPLOYEE (Fname, Minit, Lname, Ssn, Bdate, Address, Sex, Salary, Super_ssn, Dno) VALUES('James', 'E', 'Borg', 888665555, '11-NOV-1937', '450 Stone, Houston, TX', 'M', 55000, NULL, 1);
+INSERT INTO PROJECT (Pname, Pnumber, Plocation, Dnum) VALUES ('ProductX', 1, 'Bellaire', 5);
+INSERT INTO PROJECT (Pname, Pnumber, Plocation, Dnum) VALUES ('ProductY', 2, 'Sugarland', 5);
+INSERT INTO PROJECT (Pname, Pnumber, Plocation, Dnum) VALUES ('ProductZ', 3, 'Houston', 5);
+INSERT INTO PROJECT (Pname, Pnumber, Plocation, Dnum) VALUES ('Computerization', 10, 'Stafford', 4);
+INSERT INTO PROJECT (Pname, Pnumber, Plocation, Dnum) VALUES ('Reorganization', 20, 'Houston', 1);
+INSERT INTO PROJECT (Pname, Pnumber, Plocation, Dnum) VALUES ('Newbenefits', 30, 'Stafford', 4);
+INSERT INTO WORKS_ON (Essn, Pno, Hours) VALUES (123456789 , 1, 32.5);
+INSERT INTO WORKS_ON (Essn, Pno, Hours) VALUES (123456789 , 1, 32.5);
+INSERT INTO WORKS_ON (Essn, Pno, Hours) VALUES (123456789 , 1, 32.5);
+INSERT INTO WORKS_ON (Essn, Pno, Hours) VALUES (123456789 , 1, 32.5);
+INSERT INTO WORKS_ON (Essn, Pno, Hours) VALUES (123456789 , 1, 32.5);
+INSERT INTO WORKS_ON (Essn, Pno, Hours) VALUES (123456789 , 1, 32.5);
+INSERT INTO WORKS_ON (Essn, Pno, Hours) VALUES (123456789 , 1, 32.5);
+INSERT INTO WORKS_ON (Essn, Pno, Hours) VALUES (123456789 , 1, 32.5);
+INSERT INTO WORKS_ON (Essn, Pno, Hours) VALUES (123456789 , 1, 32.5);
+INSERT INTO WORKS_ON (Essn, Pno, Hours) VALUES (123456789 , 1, 32.5);
+INSERT INTO WORKS_ON (Essn, Pno, Hours) VALUES (123456789 , 1, 32.5);
+INSERT INTO WORKS_ON (Essn, Pno, Hours) VALUES (123456789 , 1, 32.5);
+INSERT INTO WORKS_ON (Essn, Pno, Hours) VALUES (123456789 , 1, 32.5);
+INSERT INTO WORKS_ON (Essn, Pno, Hours) VALUES (123456789 , 1, 32.5);
+INSERT INTO WORKS_ON (Essn, Pno, Hours) VALUES (123456789 , 1, 32.5);
+INSERT INTO WORKS_ON (Essn, Pno, Hours) VALUES (123456789 , 1, 32.5);
