499d34c073ce8ceaecb62afe42e698c6be87216e
AlexLundin <aml140830@utdallas.edu>
2018-10-07 14:04:10 -0500

    tables

--- 1.2-SE-4347.501-DB/ModelsForClass/5.8/DEVELOPER/CREATE-TABLES.sql
+++ 1.2-SE-4347.501-DB/ModelsForClass/5.8/DEVELOPER/CREATE-TABLES.sql
@@ -1,3 +1,25 @@
+--- these lines deletes existing tables
+DROP TABLE AIRPORT;
+DROP TABLE FLIGHT;
+DROP TABLE FLIGHT_LEG;
+DROP TABLE LEG_INSTANCE;
+DROP TABLE FARE;
+DROP TABLE AIRPLANE_TYPE;
+DROP TABLE CAN_LAND;
+DROP TABLE AIRPLANE;
+DROP TABLE SEAT_RESERVATION;
+
+--- these lines clear data in tables
+TRUNCATE TABLE AIRPORT;
+TRUNCATE TABLE FLIGHT;
+TRUNCATE TABLE FLIGHT_LEG;
+TRUNCATE TABLE LEG_INSTANCE;
+TRUNCATE TABLE FARE;
+TRUNCATE TABLE AIRPLANE_TYPE;
+TRUNCATE TABLE CAN_LAND;
+TRUNCATE TABLE AIRPLANE;
+TRUNCATE TABLE SEAT_RESERVATION;
+
 CREATE TABLE AIRPORT
   (Airport_code INT     	 NOT NULL,
    Name      VARCHAR(40)     NOT NULL,
@@ -12,10 +34,69 @@ CREATE TABLE FLIGHT
    PRIMARY KEY(Flight_number));
 
 CREATE TABLE FLIGHT_LEG
+  (Flight_number 				INT     	 		NOT NULL,
+   Leg_number      				INT     			NOT NULL,
+   Departure_airport_code      	INT     			NOT NULL,
+   Scheduled_departure_time     TIMESTAMP(6)     	NOT NULL,
+   Arrival_airport_code      	INT     			NOT NULL,
+   Scheduled_arrival_time     	TIMESTAMP(6)     	NOT NULL,
+   PRIMARY KEY(Flight_number, Leg_number),
+   FOREIGN KEY(Flight_number) REFERENCES FLIGHT(Flight_number),
+   UNIQUE(Leg_number)); 
+   
+   
+CREATE TABLE LEG_INSTANCE
   (Flight_number 				INT     	 	NOT NULL,
    Leg_number      				INT     		NOT NULL,
+   Date1						DATE			NOT NULL,
+   Number_of_avaiable_seats     INT     		NOT NULL,
+   Airplane_id      			INT     		NOT NULL,
    Departure_airport_code      	INT     		NOT NULL,
-   Scheduled_departure_time     TIMESTAMP     	NOT NULL,
-   Arival_airport_code      	INT     		NOT NULL,
-   Scheduled_arival_time     	TIMESTAMP     	NOT NULL,
-   PRIMARY KEY(Leg_number)); 
+   Departure_time     			TIMESTAMP(6)    NOT NULL,
+   Arrival_airport_code      	INT     		NOT NULL,
+   Arrival_time     			TIMESTAMP(6)    NOT NULL,
+   PRIMARY KEY(Flight_number, Leg_number, Date1),
+   FOREIGN KEY(Flight_number) REFERENCES FLIGHT(Flight_number),
+   FOREIGN KEY(Leg_number) REFERENCES FLIGHT_LEG(Leg_number)); 
+   
+   
+CREATE TABLE FARE
+  (Flight_number 	INT     	 	NOT NULL,
+   Fare_code 		INT     	 	NOT NULL,
+   Amount     		DECIMAL(10,2)	NOT NULL,
+   Restrictions     VARCHAR(40)     NOT NULL,
+   PRIMARY KEY(Flight_number,Fare_code),
+   FOREIGN KEY(Flight_number) REFERENCES FLIGHT(Flight_number),
+   UNIQUE(Fare_code)); 
+   
+CREATE TABLE AIRPLANE_TYPE
+  (Airplane_type_name   VARCHAR(40)     NOT NULL,
+   Max_seats 			INT     	 	NOT NULL,
+   Company     			VARCHAR(40)     NOT NULL,
+   PRIMARY KEY(Airplane_type_name));    
+   
+CREATE TABLE CAN_LAND
+  (Airport_code 		INT     	 NOT NULL,
+   Airplane_type_name   VARCHAR(40)  NOT NULL,
+   PRIMARY KEY(Airport_code,Airplane_type_name),
+   FOREIGN KEY(Airport_code) REFERENCES AIRPORT(Airport_code),
+   FOREIGN KEY(Airplane_type_name) REFERENCES AIRPLANE_TYPE(Airplane_type_name));
+
+CREATE TABLE AIRPLANE
+  (Airplane_id 				INT     	 	NOT NULL,
+   Total_number_of_seats 	INT     	 	NOT NULL,
+   Airplane_type  			VARCHAR(40)     NOT NULL,
+   PRIMARY KEY(Airplane_id)); 
+   
+CREATE TABLE SEAT_RESERVATION
+  (Flight_number 		INT     	 	NOT NULL,
+   Leg_number      		INT     		NOT NULL,
+   Date1				DATE			NOT NULL,
+   Seat_number      	INT     		NOT NULL,
+   Customer_name     	VARCHAR(40)     NOT NULL,
+   Customer_phone     	VARCHAR(40)     NOT NULL,
+   PRIMARY KEY(Flight_number,Leg_number, Date1, Seat_number),
+   FOREIGN KEY(Flight_number) REFERENCES FLIGHT(Flight_number),
+   FOREIGN KEY(Leg_number) REFERENCES FLIGHT_LEG(Leg_number),
+   UNIQUE(Seat_number));   
+   
