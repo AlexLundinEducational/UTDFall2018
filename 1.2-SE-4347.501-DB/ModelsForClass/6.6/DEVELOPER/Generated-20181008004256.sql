aa3647b36f4eda1c5186f45d465ab024dcd7aacb
AlexLundin <aml140830@utdallas.edu>
2018-10-08 02:38:08 -0500

    triggerTests

--- 1.2-SE-4347.501-DB/ModelsForClass/6.6/DEVELOPER/Generated-20181008004256.sql
+++
@@ -1,7 +0,0 @@
---------------------------------------------------------
---  File created - Monday-October-08-2018   
---------------------------------------------------------
-@C:\Users\Alex\Desktop\Screen-Cleaner\GitHub\UTDFall2018\1.2-SE-4347.501-DB\ModelsForClass\6.6\DEVELOPER\UPDATEBOOKID.sql
-@C:\Users\Alex\Desktop\Screen-Cleaner\GitHub\UTDFall2018\1.2-SE-4347.501-DB\ModelsForClass\6.6\DEVELOPER\UPDATEBRANCHID.sql
-@C:\Users\Alex\Desktop\Screen-Cleaner\GitHub\UTDFall2018\1.2-SE-4347.501-DB\ModelsForClass\6.6\DEVELOPER\UPDATECARDNO.sql
-@C:\Users\Alex\Desktop\Screen-Cleaner\GitHub\UTDFall2018\1.2-SE-4347.501-DB\ModelsForClass\6.6\DEVELOPER\UPDATEPUBLISHERNAME.sql
