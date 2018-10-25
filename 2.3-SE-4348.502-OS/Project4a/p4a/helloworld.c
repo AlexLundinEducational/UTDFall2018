e4054f45d42e965aa07bf531c6efe916d4deaa7b
AlexLundin <aml140830@utdallas.edu>
2018-10-25 00:05:37 -0500

    p4a

---
+++ 2.3-SE-4348.502-OS/Project4a/p4a/helloworld.c
@@ -0,0 +1,14 @@
+
+
+
+#include "includeFile.h"
+#include <stdio.h>
+
+// Hello world, with include file
+int main()
+{
+   // printf() displays the string inside quotation
+   printf("\nHello, World!");
+   printf("\n.");
+   return 0;
+}
---
+++ 2.3-SE-4348.502-OS/Project4a/p4a/include/includeFile.h
@@ -0,0 +1,11 @@
+#ifndef _includeFile_H_
+#define _includeFile_H_
+
+// helloworld.c
+
+// function prototypes
+
+// global variables
+
+
+#endif // _DEFS_H_
